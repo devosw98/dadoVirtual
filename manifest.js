34ca1ba2e5247f710adc3c2f11c9804bd4436b2a
devosw98 <dev.osw98cd@gmail.com>
2024-09-17 22:47:43 -0600

    mejoras y manifest.js

---
+++ manifest.js
@@ -0,0 +1,21 @@
+{
+    "name": "Caja Random",
+    "short_name": "AppWeb",
+    "description": "Un dado simple, intuitivo y útil.",
+    "start_url": "/index.html",
+    "display": "standalone",
+    "background_color": "#ffffff",
+    "theme_color": "#000000",
+    "icons": [
+      {
+        "src": "img/icon_page192.png",
+        "sizes": "192x192",
+        "type": "image/png"
+      },
+      {
+        "src": "img/icon_page512.png",
+        "sizes": "512x512",
+        "type": "image/png"
+      }
+    ]
+  }
